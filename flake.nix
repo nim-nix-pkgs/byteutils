fa455829f6a6956f6b477f551a543baf6616e501
hugosenari <hugosenari@gmail.com>
2022-05-12 11:50:17 -0300

    chore: re index byteutils

--- flake.nix
+++ flake.nix
@@ -7,6 +7,14 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
+  inputs."byteutils-master".dir   = "master";
+  inputs."byteutils-master".owner = "nim-nix-pkgs";
+  inputs."byteutils-master".ref   = "master";
+  inputs."byteutils-master".repo  = "byteutils";
+  inputs."byteutils-master".type  = "github";
+  inputs."byteutils-master".inputs.nixpkgs.follows = "nixpkgs";
+  inputs."byteutils-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
+  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
